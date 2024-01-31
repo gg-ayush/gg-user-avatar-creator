a707a0a97027ecad64c40141cfd8ec8d88bbc6a8
samy0g <gg.ayushtamang@gmail.com>
2024-01-31 16:15:09 +0545

    2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
-import { AvatarCreator } from '@readyplayerme/react-avatar-creator';
+import { AvatarCreatorConfig, AvatarCreatorEvent, AvatarCreatorRaw } from '@readyplayerme/react-avatar-creator';
+
+const config: AvatarCreatorConfig = {
+  clearCache: false,
+  bodyType: 'fullbody',
+  quickStart: false,
+  language: 'en',
+};
+
+const style = { width: '100%', height: '100vh', border: 'none' };
 
 export default function App() {
-  return <AvatarCreator subdomain="gguser" style={{ width: '100%', height: '100vh', border: 'none' }} />;
+  const handleCustomEvent = (event: AvatarCreatorEvent) => {
+    console.log(`Received custom event`, event);
+  };
+
+  return (
+    <>
+      <AvatarCreatorRaw subdomain="gguser" config={config} style={style} onEventReceived={handleCustomEvent} />
+    </>
+  );
 }
