8380d6039b235ec0440a67b55a5ab63e6632246d
samy0g <gg.ayushtamang@gmail.com>
2024-01-31 14:49:13 +0545

    1

---
+++ src/App.js
@@ -0,0 +1,5 @@
+import { AvatarCreator } from '@readyplayerme/react-avatar-creator';
+
+export default function App() {
+  return <AvatarCreator subdomain="gguser" style={{ width: '100%', height: '100vh', border: 'none' }} />;
+}
